72a083d46a16f57a16d166fd9183f6a85a967eb4
Christian Hernandez <chernand@redhat.com>
2022-01-28 08:40:36 -0800

    updated to use new gittea operator version
    Signed-off-by: Christian Hernandez <chernand@redhat.com>

--- support/containers/gitea-bootstrap/gitea-bootstrap.sh
+++ support/containers/gitea-bootstrap/gitea-bootstrap.sh
@@ -12,41 +12,15 @@ gtpass=openshift
 hookreponame=welcome-app
 eventlistener="http://el-build-listener.welcome-pipeline.svc.cluster.local:8080"
 gturl="http://gitea:3000"
+
 #
 ## Get Token
 export TOKEN=$(curl -s -X POST ${gturl}/api/v1/users/${gtadmin}/tokens -u ${gtadmin}:${gtpass} -H "Content-Type: application/json" -d '{"name": "admintoken"}' | jq -r .sha1)
 
 #
-## Create User
-echo "Creating user: ${gtuser}"
-echo "========================"
-curl -s -X POST ${gturl}/api/v1/admin/users \
--H "accept: application/json" \
--H "Authorization: token ${TOKEN}" \
--H "Content-Type: application/json" -d \
-"{\"email\":\"${gtuser}@example.com\",\"login_name\":\"${gtuser}\",\"must_change_password\":false,\"password\":\"${gtpass}\",\"send_notify\":false,\"username\":\"${gtuser}\"}"
-
-#
-## Get User ID
-export GTUID=$(curl -s -X GET ${gturl}/api/v1/users/${gtuser} -H "accept: application/json" -H "Authorization: token ${TOKEN}"  | jq -r .id)
-
-#
-## Migrate repos
-for repo in https://github.com/RedHatWorkshops/welcome-app https://github.com/RedHatWorkshops/welcome-deploy
-do
-	reponame=$(basename ${repo})
-	echo "Migrating Repo - ${reponame}"
-	echo "============================"
-	curl -s -X POST ${gturl}/api/v1/repos/migrate \
-	-H "accept: application/json" \
-	-H "Authorization: token ${TOKEN}" \
-	-H "Content-Type: application/json" -d \
-	"{\"clone_addr\":\"${repo}\",\"description\":\"\",\"issues\":false,\"milestones\":false,\"mirror\":false,\"private\":false,\"repo_name\":\"${reponame}\",\"uid\":${GTUID}}"
-done
-
-#
 ## Create developer token
 export DTOKEN=$(curl -s -X POST ${gturl}/api/v1/users/${gtuser}/tokens -u ${gtuser}:${gtpass} -H "Content-Type: application/json" -d '{"name": "develtoken"}' | jq -r .sha1)
+
 #
 ## Create hook if not there.
 echo "Creating Webhook for repo: ${hookreponame}"
